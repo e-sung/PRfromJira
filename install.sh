4758fa48ff45d518abfee5a80971e3b0e3c48b9b
Sungdoo <dev.esung@gmail.com>
2022-06-27 16:40:39 +0900

    use git branch

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-FILE="~/createPR"
+FILE="/usr/local/bin/createPR"
 if [ -f "$FILE" ]; then
     rm -rf $FILE
 fi
 
-swift build && cp .build/arm64-apple-macosx/debug/PRfromJira /usr/local/bin/createPR
+swift build && cp .build/arm64-apple-macosx/debug/PRfromJira $FILE
